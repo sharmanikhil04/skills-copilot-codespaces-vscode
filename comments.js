c2f49df38e4bbf0cd9263e4d9a30cbd97a5a7bcc
Nikhil Sharma <sharmanikhil04@gmail.com>
2024-05-30 11:06:15 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,12 @@
+const http = require('http');
+
 // create web server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
